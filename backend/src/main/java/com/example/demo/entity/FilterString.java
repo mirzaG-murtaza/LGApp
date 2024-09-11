85b4b39588b1a23435ad4a6e2ac4ed75b1d6d678
Ali Ahmad <62994046+CyberSicario47@users.noreply.github.com>
2024-09-12 01:59:16 +0500

    updated filter functionality

---
+++ backend/src/main/java/com/example/demo/entity/FilterString.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+public class FilterString {
+
+    private String filterString;
+
+
+    public String getFilterString() {
+        return filterString;
+    }
+
+    public void setFilterString(String filterString) {
+        this.filterString = filterString;
+    }
+}
